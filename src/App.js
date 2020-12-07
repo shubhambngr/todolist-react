60b117ffe62e6fa7d61edf651ba1595a5cf07da0
Shubham <shubhambangar700@gmail.com>
2020-12-07 10:11:06 +0530

    Check for empty input

--- src/App.js
+++ src/App.js
@@ -2,71 +2,75 @@ import React, { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [inputText, setInputText] = useState("");
+  const [listItems, setListItems] = useState([]);
+  const [emptyInput, setEmptyInput] = useState(false);
 
-    const [inputText, setInputText] = useState("");
-    const [listItems, setListItems] = useState([]);
-    const [emptyInput, setEmptyInput] = useState(false);
+  function handleInputChange(event) {
+    const value = event.target.value;
+    setInputText(value);
+  }
 
-    function handleInputChange(event) {
-        const value = event.target.value;
-        setInputText(value);
+  function addItem() {
+    if (inputText.trim() !== "") {
+      setListItems((prevValue) => {
+        return [...prevValue, inputText];
+      });
+      setInputText("");
+      setEmptyInput(false);
+    } else {
+      setInputText("");
+      setEmptyInput(true);
     }
+  }
 
-    function addItem() {
-        if (inputText !== "") {
-            setListItems((prevValue) => {
-              return [...prevValue, inputText];
-            });
-            setInputText("");
-            setEmptyInput(false);
-        } else {
-            setEmptyInput(true);
-        }
+  function deleteItem(event) {
+    const deleteIndex = event.target.value;
+    listItems.splice(deleteIndex, 1);
+    const filteredListItems = listItems.filter((newItems) => newItems !== "");
+    setListItems(filteredListItems);
+  }
+
+  function handleKeyPress(event) {
+    if (event.key === "Enter") {
+      addItem();
     }
+  }
 
-    function deleteItem(event) {
-        const deleteIndex = event.target.value;
-        listItems.splice(deleteIndex, 1);
-        const filteredListItems = listItems.filter(newItems => newItems !== "");
-        setListItems(filteredListItems);
-    }
-
-    function handleKeyPress(event) {
-        if (event.key === "Enter") {
-            addItem();
-        }
-    }
-
-    return (
-      <div className="container">
-        <div className="heading">
-          <h1>To Do List</h1>
-        </div>
-        <div className="form">
-          <input
-            onKeyPress={handleKeyPress}
-            onChange={handleInputChange}
-            type="text"
-            value={inputText}
-            placeholder={emptyInput ? "Empty Entry!" : "Enter Item"}
-            autoFocus
-          />
-          <button onClick={addItem}>
-            <span>Add</span>
-          </button>
-        </div>
-        <div>
-          <ul>
-            {listItems.map((item, index) => (
-              <li key={index}>
-                {item}
-                <button value={index} onClick={deleteItem} style={{float: "right"}}>
-                    Delete
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To Do List</h1>
       </div>
-    );
+      <div className="form">
+        <input
+          onKeyPress={handleKeyPress}
+          onChange={handleInputChange}
+          type="text"
+          value={inputText}
+          placeholder={emptyInput ? "Empty Entry!" : "Enter Item"}
+          autoFocus
+        />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {listItems.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button
+                value={index}
+                onClick={deleteItem}
+                style={{ float: "right" }}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
